c5070d9aab58751cb19131f4bdda41cfc8e6e9d0
AJ Sigmon <alexandersigmon@gmail.com>
2016-07-29 10:03:59 -0400

    comments are displaying in partial view and comment button is working

--- PortfolioPage/js/myscripts.js
+++ PortfolioPage/js/myscripts.js
@@ -1,6 +1,14 @@
 ﻿$(document).ready(function () {
 
 
+    $("#blog-comments-show-hide").click(function () {
+        $(".blogpost-comment-section").slideToggle("fast", function () {
+            $(".comment-arrow-span").html(function(i, v){
+                return v === '<i class="fa fa-angle-double-down" aria-hidden="true"></i>' ? '<i class="fa fa-angle-double-up" aria-hidden="true"></i>' : '<i class="fa fa-angle-double-down" aria-hidden="true"></i>'
+            })
+        });
+        
+    });
 
 
 });
